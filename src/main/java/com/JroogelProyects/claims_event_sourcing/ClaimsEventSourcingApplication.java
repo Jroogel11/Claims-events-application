de1f949459658f34d27182160b6e692a38ddd329
Jose <josejuanrogelruiz11@gmail.com>
2026-06-09 00:20:59 +0200

    Revert "pipeline fixed"
    This reverts commit b0276318bcd8274d7e845b59daad535569000f46.

---
+++ src/main/java/com/JroogelProyects/claims_event_sourcing/ClaimsEventSourcingApplication.java
@@ -0,0 +1,13 @@
+package com.JroogelProyects.claims_event_sourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsEventSourcingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimsEventSourcingApplication.class, args);
+	}
+
+}
